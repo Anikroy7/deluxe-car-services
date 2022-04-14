0f2a7fe4a50e36efce93f1ab737a19e9db64553e
Anikroy7 <roy645800@gmail.com>
2022-04-14 13:56:23 +0600

    login component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './pages/shared/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/About/About';
 import Home from './pages/Home/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
