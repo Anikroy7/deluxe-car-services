80adcf625090666bb97aecd3c5c3dde581ed3678
Anikroy7 <roy645800@gmail.com>
2022-04-14 18:31:02 +0600

    register component added with pure css

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -16,13 +16,12 @@ const Login = () => {
         console.log(email, password);
 
     }
-    const handelRegister = () => {
-
+    const handelNavigateToRegister = () => {
         navigate('/register')
     }
 
     return (
-        <Form className='w-50 border m-4 p-4 rounded mx-auto'>
+        <Form onSubmit={handleLogIn} className='w-50 border m-4 p-4 rounded mx-auto'>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
@@ -39,11 +38,11 @@ const Login = () => {
                 <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
 
-            <p>New to Deluxe car service? <span className='sign-up-btn' onClick={handelRegister}>Register</span></p>
-            <Button onClick={handleLogIn} variant="secondary" type="submit">
+            <p>New to Deluxe car service? <span className='sign-up-btn' onClick={handelNavigateToRegister}>Register please</span></p>
+            <Button variant="secondary" type="submit">
                 Submit
             </Button>
-        </Form>
+        </Form >
     );
 };
 
--- src/pages/Login/Register/Register.js
+++ src/pages/Login/Register/Register.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import './Register.css'
 const Register = () => {
+
+    const navigate = useNavigate()
+
+
+    const handelNavigateToLogin = () => {
+
+        navigate('/login')
+    }
+
+    const handelRegister = event => {
+        event.preventDefault()
+    }
     return (
-        <div>
-            <h3>this is register</h3>
+        <div className='resgister-main-container'>
+            <h1 className='register-title'>Register Please</h1>
+            <form onSubmit={handelRegister} className='register-container'>
+                <input className='px-2' type="text" name="name" placeholder='Your name' id="" />
+                <input className='px-2' type="email" name="email" placeholder='Your email' id="" />
+                <input className='px-2' type="password" name="password" placeholder='Your password' id="" />
+                <p>Already have an account? <small className='sign-up-btn' onClick={handelNavigateToLogin}>Login </small></p>
+                <input className='register-btn' type="submit" value="Register" />
+            </form>
         </div>
     );
 };
